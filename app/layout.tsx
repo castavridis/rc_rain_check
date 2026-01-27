49d0d39ace83c9d174f2b56c963ffe380ac285cd
C Stavridis <castavridis@gmail.com>
2026-01-27 00:54:46 -0600

    Add CSS  gradient experiments to FE

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,22 @@ import './globals.css'
 export default function RootLayout (
   { children }: { children: React.ReactNode }
 ): React.ReactElement {
+
   return (
     <html lang="en">
       <body>
-        <UserNav />
-        <LoginButton />
-        <RainCheckForm />
-        {children}
+        <div className="gradient-background dawn-dusk fixed inset-0 z-0" aria-hidden="true">
+          <div className="gradient-bg absolute inset-0" aria-hidden="true"></div>
+          <div className="gradient-mg absolute inset-0" aria-hidden="true"></div>
+          <div className="gradient-fg absolute inset-0" aria-hidden="true"></div>
+          <div className="noise absolute inset-0" aria-hidden="true"></div>
+        </div>
+        <div className="relative z-10">
+          <UserNav />
+          <LoginButton />
+          <RainCheckForm />
+          {children}
+        </div>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+"use client"
 export default function Page(): React.ReactElement {
-  return <h1>Hello, Next.js!</h1>
+  function handleClick(mode: string) {
+    const gradientBg = document.querySelector('.gradient-background')
+    gradientBg.classList.remove("day", "dawn-dusk", "evening")
+    gradientBg.classList.add(mode)
+  }
+  return (
+    <div className="flex gap-2 mt-2">
+      <button className="bg-white text-black border rounded-md p-2" onClick={ () => handleClick("day") }>Day</button>
+      <button className="bg-white text-black border rounded-md p-2" onClick={ () => handleClick("dawn-dusk") }>Dawn/Dusk</button>
+      <button className="bg-white text-black border rounded-md p-2" onClick={ () => handleClick("evening") }>Night</button>
+    </div>
+  )
 }
