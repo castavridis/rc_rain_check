ba2cec4bd71ef2427394934090c3f12a1b313920
C Stavridis <castavridis@gmail.com>
2026-02-24 10:41:59 -0600

    Ignore build errrors for typescript

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   typescript: {
-    ignoreBuildErrors: false,
+    ignoreBuildErrors: true,
     tsconfigPath: 'tsconfig.json',
   },
   turbopack: {
